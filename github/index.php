c01dba5bec67b78bfd9c0a8aa42c38be8c233789
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-10 15:38:19 +0300

    Add files via upload

---
+++ github/index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css\bootstrap.css">
+</head>
+<body>
+<div class="section">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-lg-offset-3">
+              <div class="header text-center">
+                  <span>Получите видео-инструкцию</span>
+                  <strong class="text-uppercase">Как заработать свой первый миллион</strong>
+              </div>
+          </div>
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-1 col-md-8 col-md-offset-2">
+                <div class="video">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/U9X5LFTGZBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+        <div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2">
+            <div class="form">
+                <p class="form-header text-center">Введите свой email</p>
+                <form action="#">
+                    <input type="text" class="form-control input-lg" placeholder="Ваше имя">
+                    <input type="text" class="form-control input-lg" placeholder="Ваш номер телефона">
+                    <div class="btn btn-primary btn-block btn-lg btn-form">Получить инструкцию</div>
+                </form>
+                    </div>
+             </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="container">
+        <!-- row под заголовок секции-->
+        <div class="row">
+            <div class="col-lg-12 col-xs-12 col-sm-12">
+                <h2 class = "section-header text-uppercase text-center">Почему нужно сделать это сейчас</h2>
+            </div>
+        </div>
+        <!-- блок с преимуществами-->
+        <div class="feature text-center">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="feature-box"><img src="img/1.svg" alt="1 преимущество">
+                        <p>Первое преимущество</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="feature-box"><img src="img/2.svg" alt="2 преимущество">
+                        <p>Второе преимущество</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="feature-box"><img src="img/3.svg" alt="3 преимущество">
+                        <p>Третье преимущество</p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="feature-box"><img src="img/4.svg" alt="4 преимущество">
+                        <p>Четвертое преимущество</p>
+                    </div>
+                </div>
+            </div>
+            <div class="btn btn-primary btn-lg btn-form">Получить инструкцию</div>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
